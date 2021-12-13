98b97f851a59cd3653a1e5dfaa89c5054ac44ce3
arkamahamed-git <arkamahamed88@gmail.com>
2021-12-13 17:23:16 +0530

    docker

--- src/main/java/com/mongoDB/mongoDB/dao/StudentRepository.java
+++ src/main/java/com/mongoDB/mongoDB/dao/StudentRepository.java
@@ -2,11 +2,10 @@ package com.mongoDB.mongoDB.dao;
 
 import com.mongoDB.mongoDB.model.Student;
 import org.bson.types.ObjectId;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
+
 import java.util.List;
 import java.util.UUID;
 
